ec6ce1ec3b877aecdc5b7a9f02014cbabb3b7b6e
Bernardo Soeiro <bernardo.d.soeiro@gmail.com>
2025-12-02 22:49:36 +0000

    cold

--- app/server.py
+++ app/server.py
@@ -1,10 +1,9 @@
 import logging
-from app import APP
+from app import app
 import db
 
 if __name__ == '__main__':
   logging.basicConfig(level=logging.INFO,
                     format='%(asctime)s - %(levelname)s - %(message)s',
                     datefmt='%Y-%m-%d %H:%M:%S')
-  db.connect()
-  APP.run(host='0.0.0.0', port=9000)
+  app.run(host='0.0.0.0', port=9000)
